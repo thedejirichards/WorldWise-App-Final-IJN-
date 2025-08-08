fd3b8f16be266a41de83e1a5dee37ad219f1d152
Richard  Oladeji <oladejir@accessbankplc.com>
2025-08-08 08:16:51 +0100

    Pushing first local to github

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav"
+
+function HomePage() {
+    return (
+        <div>
+            <PageNav/>
+            <h1>Worldwise App</h1>
+        </div>
+    )
+}
+
+export default HomePage
