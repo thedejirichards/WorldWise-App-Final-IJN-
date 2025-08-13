cb7bbe21a4ca94194f484ff466d52a72d302b0b5
Richard  Oladeji <oladejir@accessbankplc.com>
2025-08-13 09:03:42 +0100

    After adding starter data

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -11,6 +13,13 @@ function App() {
         <Route path="/" element= {<HomePage/>}/>
         <Route path="product" element= {<Product/>}/>
         <Route path="pricing" element= {<Pricing/>}/>
+        <Route path="login" element= {<Login/>}/>
+        <Route path="app" element= {<AppLayout/>}>
+          <Route index element={<p>List of cities</p>}/>
+          <Route path="cities" element= {<p>List of cities</p>}/>
+          <Route path="countries" element= {<p>List of countries</p>}/>
+          <Route path="form" element= {<p>Form</p>}/>
+        </Route>
         <Route path="*" element= {<PageNotFound/>}/>
       </Routes>
     </BrowserRouter>
--- src/components/PageNav.tsx
+++ src/components/PageNav.tsx
@@ -1,21 +1,26 @@
-import { Link } from "react-router-dom"
-
+import { NavLink } from "react-router-dom";
+import styles from "../components/PageNav.module.css";
+import Logo from "./Logo";
 function PageNav() {
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/pricing">Pricing</Link>
-                </li>
-                <li>
-                    <Link to="/product">Product</Link>
-                </li>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav className={styles.nav}>
+      <Logo />
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/pricing">Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to="/product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className={styles.ctaLink}>Login</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default PageNav
+export default PageNav;
